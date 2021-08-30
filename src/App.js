2d88af5d3dc23c826e598babc76b31939d35a0f8
Jung-Ah-C <wjddk718@gmail.com>
2021-08-31 00:37:41 +0900

    Router, Navigation, CSS 추가

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
 import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 import "./App.css";
+import Navigation from "./components/Navigation";
 
-// setState를 호출할 때마다 react는 새로운 state와 함께 render함수를 호출한다. (리렌더링 작업을 해줌)
-
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: []
-    };
-    // axios를 통해서 데이터를 가져오는데에 시간이 조금 걸리기 때문에 async와 await를 사용해서 자바스크립트에게 알려줘야
-    getMovies = async () => {
-        const {
-            data: {
-                data:{ movies }
-            }
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-        this.setState({ movies, isLoading: false });
-    };
-    componentDidMount() {
-        this.getMovies();
-    }
-
-    render() {
-        const { isLoading, movies } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                      <span className="loader_text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map(movie => (
-                            <Movie
-                                key={movie.id}
-                                id={movie.id}
-                                year={movie.year}
-                                title={movie.title}
-                                summary={movie.summary}
-                                poster={movie.medium_cover_image}
-                                genres={movie.genres}
-                            />
-                        ))}
-                    </div>
-                )}
-            </section>
-        );
-    }
+function App() {
+    return (
+        <BrowserRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
